81b040b1dfdc55a346011508f3bfd1668975cbb3
etrinhh <etrinhh@gmail.com>
2021-03-10 14:30:03 -0700

    changed stuff sos

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const path = require("path");
 const express = require("express");
 const app = express();
 
-app.use(express.static(path.join(__dirname, public)));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(function (request, response) {
   response.status(404);
