7780cae431724214e18a3fc854a9511ea9a89c4a
etrinhh <etrinhh@gmail.com>
2021-03-11 13:58:48 -0700

    commented code

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
+// initiate new project, import express
 const path = require("path");
 const express = require("express");
 const app = express();
 
+//use project in public folder
 app.use(express.static(path.join(__dirname, "public")));
 
+//create 404 page 
 app.use(function (request, response) {
   response.status(404);
   response.send("404: File IS LITERALLY NOT THERE OK");
 });
-
+//server port, use 3000 when process.env not found
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, function () {
   console.log(`listening on port: ${PORT}`);
